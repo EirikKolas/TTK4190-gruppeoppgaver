2528963e40ae251230a53e262e9f3ac1f7cd3d14
Eirik Kolås <kolaseirik@gmail.com>
2023-10-24 12:20:45 +0200

    fixed reference model, need refernce

--- matlab/oving2.2/ref_model.m
+++ matlab/oving2.2/ref_model.m
@@ -10,9 +10,7 @@ zeta = 0.7;
 % Third order reference model between psi_ref and psi_d
 sys = omega_n^3/((s + omega_n)*(s^2 + 2*zeta*omega_n*s + omega_n^2));
 
-ss_model = tf2ss(sys.Numerator{1}, sys.Denominator{1});
-A = ss_model{1};
-B = ss_model{2};
+[A, B] = tf2ss(sys.Numerator{1}, sys.Denominator{1});
 
 xd_dot = A*xd + B*psi_ref;
 
